f6131c3f75ee2465f7bf39e2e1ff743fe0ca575c
Juan Pablo Sanchez Moreno <96924214+jpsanchezm1@users.noreply.github.com>
2023-06-04 22:46:54 -0500

    aniadir detalles pagos y consumos (#21)

--- entrega1/appHotel/modelo/pagos/ControladorPagos.java
+++ entrega1/appHotel/modelo/pagos/ControladorPagos.java
@@ -14,30 +14,30 @@ public class ControladorPagos {
 
 	private String archivoPagos = "./data/consumos/pagos.txt";
 	
-	private String archivoPagosReservas = "./data/consumos/pagosReserva.txt";
+	private String archivoPagosReservas = "./data/consumos/pagosReservas.txt";
 
 	public ControladorPagos() throws IOException {
 		recuperarInformacion();
 	}
 
-	public void pagarConsumo(String ide, String referencia) {
+	public void pagarConsumo(String ide, String referencia, String precio) {
 		LocalTime tiempoActual = LocalTime.now();
 		Integer id = Integer.parseInt(ide);
 		Pago pago = new Pago(id, referencia, tiempoActual);
 		pagosPorHuesped.computeIfAbsent(id, k -> new ArrayList<>());
 		pagosPorHuesped.get(id).add(pago);
 		EditorPagos editorPagos = new EditorPagos();
-		String infoPago = id.toString() + ";" + referencia + ";" + tiempoActual.toString();
+		String infoPago = id.toString() + ";" + referencia + ";" + tiempoActual.toString() + ";" + precio;
 		editorPagos.registarPago(archivoPagos, infoPago);
 	}
 
-	public void pagarReserva(String ide, String referencia) {
+	public void pagarReserva(String ide, String referencia, String precio) {
 		LocalTime tiempoActual = LocalTime.now();
 		Integer id = Integer.parseInt(ide);
 		Pago pago = new Pago(id, referencia, tiempoActual);
 		pagosDeReserva.put(referencia, pago);
 		EditorPagos editorPagos = new EditorPagos();
-		String infoPago = id.toString() + ";" + referencia + ";" + tiempoActual.toString();
+		String infoPago = id.toString() + ";" + referencia + ";" + tiempoActual.toString() + ";" + precio;
 		editorPagos.registarPago(archivoPagosReservas, infoPago);
 	}
 
